7a23968d9f53e2c068c78259ffe7f02b44620d18
David Schmitz <david.schmitz.privat@gmail.com>
2017-12-20 15:49:23 +0100

    Initial app

---
+++ FIS 2.0 Mobile CenterTests/FIS_2_0_Mobile_CenterTests.swift
@@ -0,0 +1,36 @@
+//
+//  FIS_2_0_Mobile_CenterTests.swift
+//  FIS 2.0 Mobile CenterTests
+//
+//  Created by David Schmitz on 20.12.17.
+//  Copyright © 2017 org.koenighotze. All rights reserved.
+//
+
+import XCTest
+@testable import FIS_2_0_Mobile_Center
+
+class FIS_2_0_Mobile_CenterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
